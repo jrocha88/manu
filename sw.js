36d292f29f727c0fe911a19e21c35e5176f75720
jrocha88 <j.roberto_rocha@hotmail.com>
2025-10-15 16:48:07 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE = 'picturebee-v1';
+const CACHE = 'picturebee-v2';
 const ASSETS = ['.', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
